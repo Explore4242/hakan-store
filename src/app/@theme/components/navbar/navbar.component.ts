803b003c8a77d71b1d448a4abbbc834b9656f047
Hakan Bağlan <baglanhakan42@gmail.com>
2021-10-05 19:08:09 +0300

    navbara kategori eklendi

--- src/app/@theme/components/navbar/navbar.component.ts
+++ src/app/@theme/components/navbar/navbar.component.ts
@@ -6,7 +6,10 @@ import { filter, map } from 'rxjs/operators';
 import { UserData } from '../../../@core/data/users';
 import { LayoutService } from '../../../@core/utils';
 import { RippleService } from '../../../@core/utils/ripple.service';
+import { Product } from '../../../pages/model/product';
 import { AuthService } from '../../../pages/services/auth.service';
+import { CategoryService } from '../../../pages/services/category.service';
+import { ProductService } from '../../../pages/services/product.service';
 
 @Component({
   selector: 'ngx-navbar',
@@ -14,15 +17,17 @@ import { AuthService } from '../../../pages/services/auth.service';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
- 
+
   private destroy$: Subject<void> = new Subject<void>();
   public readonly materialTheme$: Observable<boolean>;
   userPictureOnly: boolean = false;
   user: any;
-
+  categories: string[];
+  products: Product[];
+  productDetail: Product;
 
   constructor(private router: Router,
-    private nbMenuService: NbMenuService, 
+    private nbMenuService: NbMenuService,
     private sidebarService: NbSidebarService,
     private menuService: NbMenuService,
     private themeService: NbThemeService,
@@ -31,37 +36,40 @@ export class NavbarComponent implements OnInit {
     private breakpointService: NbMediaBreakpointsService,
     private rippleService: RippleService,
     public authService: AuthService,
-   ) {
+    private categoryService: CategoryService,
+    private productService: ProductService,
+  ) {
     this.menuService.onItemClick()
-    .pipe(
-      filter(({ tag }) => tag === 'mainMenu'),
-      map(({ item }) => item),
-    )
-    .subscribe((item: any) => {
-      console.log('Menüye tıklandı', item)
-      if (item.title === 'Profile') {
-         this.router.navigate(['pages','profil'])
-      } else if (item.title === 'Log out') {
-        return this.authService.logouth();
-      }
-    });
+      .pipe(
+        filter(({ tag }) => tag === 'mainMenu'),
+        map(({ item }) => item),
+      )
+      .subscribe((item: any) => {
+        console.log('Menüye tıklandı', item)
+        if (item.title === 'Profile') {
+          this.router.navigate(['pages', 'profil'])
+        } else if (item.title === 'Log out') {
+          return this.authService.logouth();
+        }
+      });
 
 
-  this.materialTheme$ = this.themeService.onThemeChange()
-    .pipe(map(theme => {
-      const themeName: string = theme?.name || '';
-      return themeName.startsWith('material');
-    }));
-    }
+    this.materialTheme$ = this.themeService.onThemeChange()
+      .pipe(map(theme => {
+        const themeName: string = theme?.name || '';
+        return themeName.startsWith('material');
+      }));
+  }
 
   ngOnInit(): void {
 
     this.nbMenuService.onItemClick()
       .pipe(
         filter(({ tag }) => tag === 'nav-menu'),
-        map(({ item}) => item),
+        map(({ item }) => item),
       )
       .subscribe(item => { this.router.navigate([item.url]) });
+    this.category();
 
   }
 
@@ -88,4 +96,21 @@ export class NavbarComponent implements OnInit {
   navigateShop() {
     this.router.navigate(['pages', 'shop']);
   }
+
+  category() {
+    this.categoryService.getcategories().subscribe(res => {
+      this.categories = res;
+    });
+  }
+
+
+  getProductByCategory(category: string) {
+    this.router.navigate(['pages','anasayfa'], {
+      queryParams: {
+        category,
+      }
+    })
+  }
+
+
 }
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { threadId } from 'worker_threads';
 import { Product } from '../model/product';
 import { AuthService } from '../services/auth.service';
@@ -18,17 +18,29 @@ export class DashboardComponent implements OnInit {
   products: Product[];
   filterText = "";
   categories: string[];
+  selectedCategory = '';
 
   constructor(private productService: ProductService,
     private router: Router,
     public shopService: ShopService,
     public authService: AuthService,
-    private categoryService: CategoryService) {
-
+    private categoryService: CategoryService,
+    private acRoute: ActivatedRoute) {
+    this.router.events.subscribe(data => {
+      if (data instanceof NavigationEnd) {
+        this.acRoute.queryParams.subscribe(res => {
+          if (res.category) {
+            this.getProductByCategory(res.category);
+          } else {
+            this.getLimitProduct();
+          }
+        })
+      }
+    });
   }
 
   ngOnInit(): void {
-    this.getLimitProduct();
+    // this.getLimitProduct();
     this.category();
   }
 
@@ -53,9 +65,16 @@ export class DashboardComponent implements OnInit {
 
 
   getProductByCategory(category: string) {
-    this.productService.getProductByCategory(category).subscribe(res => {
-      this.products = res;
-    })
+    if (this.selectedCategory != category) {
+      this.selectedCategory = category;
+      this.productService.getProductByCategory(category).subscribe(res => {
+        if(res.length == 0) {
+          this.getLimitProduct();
+        } else {
+          this.products = res;
+        }
+      })
+    }
   }
 
 }
